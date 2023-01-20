f43f4190a6b649b4ff93fb39e52160f333f36f95
AlexPegg <alexpeggsailing@gmail.com>
2023-01-20 17:39:38 +0000

    build working

--- out/src/vhdlMode.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VHDL_MODE = void 0;
-exports.VHDL_MODE = {
-    language: 'vhdl',
-    scheme: 'file'
-};
-//# sourceMappingURL=vhdlMode.js.map
